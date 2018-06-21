7fa7ea6b756434dea5cae5fb9e89f4d5e99288bb
parshva45 <parshva45@gmail.com>
2018-06-21 16:36:19 -0400

    updating recipe schema

--- models/recipe/recipe.schema.server.js
+++ models/recipe/recipe.schema.server.js
@@ -1,10 +1,15 @@
 var mongoose = require('mongoose');
 
 var recipeSchema = mongoose.Schema({
-    name: String,
-    ingredients: String,
-    imageUrl: String,
-    yummlyId: String
+  name: String,
+  ingredients: String,
+  imageUrl: String,
+  yield: String,
+  totalTime: String,
+  numberOfServings: Number,
+  course: String,
+  yummlyRating: Number,
+  yummlyId: String
 }, {collection: 'recipe'});
 
 module.exports = recipeSchema;
